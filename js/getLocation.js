6fd9855f20b1328604d99cfadce8bfe9815c4109
Thays Omine <thaysomine@gmail.com>
2022-03-04 20:19:16 -0300

    add show error function

--- js/getLocation.js
+++ js/getLocation.js
@@ -11,7 +11,12 @@ function getUserPosition() {
   }
 
 function showError(){
-
+  let locationInput = document.querySelector("input").value;
+  console.log(locationInput);
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&appid=b67a9b270cd29faea4ee61edb7d0b5dc`;
+  fetch(url).then((data) => {
+    console.log(data.json());
+});
 }
 
 
