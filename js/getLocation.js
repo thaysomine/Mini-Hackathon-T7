e45a567dcaa27a2746bc0d5006d296a496cb88db
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-03-04 18:42:24 -0300

    first implementations

--- js/getLocation.js
+++ js/getLocation.js
@@ -1,30 +1,30 @@
-// function getUserPosition() {
-//     let url;
-//     navigator.geolocation.getCurrentPosition((pos) => {
-//       let lat = pos.coords.latitude;
-//       let long = pos.coords.longitude;
-//       url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=b67a9b270cd29faea4ee61edb7d0b5dc`;
-//       fetchApi(url);
-//     });
-//   }
+function getUserPosition() {
+    let url;
+    navigator.geolocation.getCurrentPosition((pos) => {
+      let lat = pos.coords.latitude;
+      let long = pos.coords.longitude;
+      url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=b67a9b270cd29faea4ee61edb7d0b5dc`;
+      fetchApi(url);
+    });
+  }
 
   
-// function fetchApi(url) {
-//     let city = document.querySelector('.city');
-//     let temp = document.querySelector('span');
-//     fetch(url)
-//     .then((data) => {
-//       return data.json();
-//     })
-//     .then((data) => {
-//       let tempInCelsius = ((5/9) * (data.main.temp-32)).toFixed(1);
-//       city.innerText = `Hoje a temperatura em ${data.name} é:`;
-//       temp.innerText = tempInCelsius;
-//     })
-//     .catch((err) => {
-//       city.innerText = `Impossível acessar o OpenWeather. Verifique a sua conexão.`;
-//       temp.innerText = `-`;
-//     })
-//   }
+function fetchApi(url) {
+    let city = document.querySelector('.city');
+    let temp = document.querySelector('span');
+    fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      let tempInCelsius = ((5/9) * (data.main.temp-32)).toFixed(1);
+      city.innerText = `Hoje a temperatura em ${data.name} é:`;
+      temp.innerText = tempInCelsius;
+    })
+    .catch((err) => {
+      city.innerText = `Impossível acessar o OpenWeather. Verifique a sua conexão.`;
+      temp.innerText = `-`;
+    })
+  }
   
-//   getUserPosition();
+  getUserPosition();
