0cdae2a7ec0c6f3af60e49b90a033126fbed7daa
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-03-04 19:00:25 -0300

    ajusts

--- js/getLocation.js
+++ js/getLocation.js
@@ -1,30 +1,33 @@
 function getUserPosition() {
-    let url;
-    navigator.geolocation.getCurrentPosition((pos) => {
-      let lat = pos.coords.latitude;
-      let long = pos.coords.longitude;
-      url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=b67a9b270cd29faea4ee61edb7d0b5dc`;
-      fetchApi(url);
-    });
+    let responseAPI
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let lat = position.coords.latitude
+            let long = position.coords.longitude
+            responseAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&lang=pt_br&APPID=b67a9b270cd29faea4ee61edb7d0b5dc`;
+            (responseAPI)
+            console.log(responseAPI);
+          });
+    }
+    // Se recursar a localição 
   }
 
   
-function fetchApi(url) {
-    let city = document.querySelector('.city');
-    let temp = document.querySelector('span');
-    fetch(url)
+function renderWeather(responseAPI) {
+    let city = document.querySelector('.city')
+    let temp = document.querySelector('span')
+    fetch(responseAPI)
     .then((data) => {
-      return data.json();
+      return data.json()
     })
     .then((data) => {
-      let tempInCelsius = ((5/9) * (data.main.temp-32)).toFixed(1);
-      city.innerText = `Hoje a temperatura em ${data.name} é:`;
-      temp.innerText = tempInCelsius;
+      let tempInCelsius = ((5/9) * (data.main.temp-32)).toFixed(1)
+      city.innerText = `Hoje a temperatura em ${data.name} é:`
+      temp.innerText = tempInCelsius
     })
     .catch((err) => {
-      city.innerText = `Impossível acessar o OpenWeather. Verifique a sua conexão.`;
-      temp.innerText = `-`;
+      city.innerText = `Impossível acessar o OpenWeather. Verifique a sua conexão.`
+      temp.innerText = `-`
     })
   }
   
-  getUserPosition();
