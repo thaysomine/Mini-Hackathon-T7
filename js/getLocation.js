fffc1517c72a2566028ad112de1f8723aca57e08
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-03-04 19:34:47 -0300

    ajusts

--- js/getLocation.js
+++ js/getLocation.js
@@ -1,29 +1,36 @@
 function getUserPosition() {
-    let responseAPI
+    let APIResponse
+    teste = document.querySelector("input").value
+    console.log(teste);
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
             let lat = position.coords.latitude
             let long = position.coords.longitude
-            responseAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&lang=pt_br&APPID=b67a9b270cd29faea4ee61edb7d0b5dc`;
-            (responseAPI)
-            console.log(responseAPI);
+            APIResponse = `https://api.openweathermap.org/data/2.5/weather?q=${teste}&units=imperial&lang=pt_br&APPID=b67a9b270cd29faea4ee61edb7d0b5dc`;
+            renderWeather(APIResponse)
           });
     }
     // Se recursar a localição 
+    else{
+      APIResponse = `https://api.openweathermap.org/data/2.5/weather?q=${teste}&units=imperial&lang=pt_br&APPID=b67a9b270cd29faea4ee61edb7d0b5dc`
+    }
   }
 
   
-function renderWeather(responseAPI) {
+function renderWeather(APIResponse) {
     let city = document.querySelector('.city')
     let temp = document.querySelector('span')
-    fetch(responseAPI)
+    let weatherDescription = document.querySelector("h3")
+    fetch(APIResponse)
     .then((data) => {
       return data.json()
     })
     .then((data) => {
-      let tempInCelsius = ((5/9) * (data.main.temp-32)).toFixed(1)
-      city.innerText = `Hoje a temperatura em ${data.name} é:`
-      temp.innerText = tempInCelsius
+      let tempCelsius = ((5/9) * (data.main.temp-32)).toFixed(1)
+      city.innerText = `${data.name}`
+      console.log(data);
+      temp.innerText = tempCelsius
+      weatherDescription.innerText = `${data.weather.description}`
     })
     .catch((err) => {
       city.innerText = `Impossível acessar o OpenWeather. Verifique a sua conexão.`
