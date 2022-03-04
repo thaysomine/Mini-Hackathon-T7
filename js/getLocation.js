b56f01d8c13920de4c73ce529badd83c79da509d
João Araujo <jjvictorac@gmail.com>
2022-03-04 19:43:21 -0300

    update layout

--- js/getLocation.js
+++ js/getLocation.js
@@ -3,23 +3,29 @@ function getUserPosition() {
     navigator.geolocation.getCurrentPosition((pos) => {
       let lat = pos.coords.latitude;
       let long = pos.coords.longitude;
-      url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=b67a9b270cd29faea4ee61edb7d0b5dc`;
+      url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&lang=pt_br&APPID=b67a9b270cd29faea4ee61edb7d0b5dc`
+      ;
       fetchApi(url);
     });
   }
 
   
 function fetchApi(url) {
-    let city = document.querySelector('.city');
+    let city = document.querySelector('#city');
     let temp = document.querySelector('span');
+    let icon = document.querySelector('#icon');
+    let description = document.querySelector('#description');
     fetch(url)
     .then((data) => {
       return data.json();
     })
     .then((data) => {
       let tempInCelsius = ((5/9) * (data.main.temp-32)).toFixed(1);
-      
+
       city.innerText = `${data.name}`;
+      description.innerText = `${data.weather[0].description}`;
+
+      icon.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
 
       temp.innerText = tempInCelsius;
     })
@@ -30,3 +36,7 @@ function fetchApi(url) {
   }
   
   getUserPosition();
+
+  function getIconUrl() {
+
+  }
